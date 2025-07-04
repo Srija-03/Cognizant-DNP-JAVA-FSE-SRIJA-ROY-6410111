d03f5dbb9bca1663f9d060435f749a0a16ee6f23
Srija Roy <143420722+Srija-03@users.noreply.github.com>
2025-07-04 08:44:00 +0530

    Add files via upload

---
+++ Week_3/spring-data-jpa-handson/Difference between JPA, Hibernate and Spring Data JPA/src/main/java/com/cognizant/hibernate_demo/HibernateUtil.java
@@ -0,0 +1,27 @@
+package com.cognizant.hibernate_demo;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        // Close caches and connection pools
+        getSessionFactory().close();
+    }
+}
